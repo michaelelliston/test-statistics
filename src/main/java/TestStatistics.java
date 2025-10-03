c69cd26273d015dc71fb01acf2a032b2f2141a1e
Michael <michael.elliston95@gmail.com>
2025-10-03 13:37:18 -0500

    added prints for each required variable

--- src/main/java/TestStatistics.java
+++ src/main/java/TestStatistics.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class TestStatistics {
+    public static void main(String[] args) {
+        int sum = 0;                                                 // declaring variables
+        double average = 0;
+        int highScore = 0;
+        int lowScore = 0;
+        int i = 0;
+
+        int[] testScores = {92, 80, 65, 99, 74, 79, 83, 67, 90, 52}; // assigning values
+        Arrays.sort(testScores);                                     // sorting array by numeric value
+
+        // System.out.println(Arrays.toString(testScores));          // prints a String version of the array; used to check ordering, not required.
+
+        while (i < testScores.length) {                              // goes through the array, adding each value to the sum
+            sum += testScores[i];
+            i++;
+        }
+
+        average = (double) sum / testScores.length;                  // calculates and assigns the average
+        highScore = testScores[testScores.length - 1];               // assigns the last value in the array
+        lowScore = testScores[0];                                    // assigns hte first value in the array
+
+        System.out.println("The average of all 10 test scores is: " + average + '.');        // prints average
+        System.out.println("The highest of all 10 test scores is: " + highScore + '.');      // prints highest score
+        System.out.println("The lowest of all 10 test scores is: " + lowScore + '.');        // prints lowest score
+    }
 }
